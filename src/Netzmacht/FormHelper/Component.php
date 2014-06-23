06462c4370e06d861114150ad8923eee08410526
David Molineus <mail@netzmacht.de>
2014-06-23 13:11:50 +0200

    fix missing attributes

--- src/Netzmacht/FormHelper/Component.php
+++ src/Netzmacht/FormHelper/Component.php
@@ -4,29 +4,8 @@ namespace Netzmacht\FormHelper;
 
 use Netzmacht\Html\Attributes;
 
+
 class Component extends Attributes
 {
-	/**
-	 * @var Attributes
-	 */
-	protected $attributes;
 
-
-	/**
-	 * @param array $attributes
-	 */
-	function __construct(array $attributes = array())
-	{
-		$attributes = array_merge(array('class' => array()), $attributes);
-		$this->attributes = new Attributes($attributes);
-	}
-
-
-	/**
-	 *
-	 */
-	public function __clone()
-	{
-		$this->attributes = clone $this->attributes;
-	}
 } 
