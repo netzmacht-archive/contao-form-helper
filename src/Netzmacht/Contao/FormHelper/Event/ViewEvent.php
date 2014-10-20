129e2ce0110c76cfead979f03338e333a0ba203c
David Molineus <mail@netzmacht.de>
2014-10-20 11:22:03 +0200

    add shortcut getters to the view event

--- src/Netzmacht/Contao/FormHelper/Event/ViewEvent.php
+++ src/Netzmacht/Contao/FormHelper/Event/ViewEvent.php
@@ -27,4 +27,52 @@ class ViewEvent extends Event
     {
         return $this->view;
     }
+
+    /**
+     * @return \Widget
+     */
+    public function getWidget()
+    {
+        return $this->view->getWidget();
+    }
+
+    /**
+     * @return \FormModel|null
+     */
+    public function getFormModel()
+    {
+        return $this->view->getFormModel();
+    }
+
+    /**
+     * @return \Netzmacht\Html\Attributes
+     */
+    public function getAttributes()
+    {
+        return $this->view->getAttributes();
+    }
+
+    /**
+     * @return \Netzmacht\Contao\FormHelper\Partial\Container
+     */
+    public function getContainer()
+    {
+        return $this->view->getContainer();
+    }
+
+    /**
+     * @return \Netzmacht\Contao\FormHelper\Partial\Label
+     */
+    public function getLabel()
+    {
+        return $this->view->getLabel();
+    }
+
+    /**
+     * @return \Netzmacht\Contao\FormHelper\Partial\Errors
+     */
+    public function getErrors()
+    {
+        return $this->view->getErrors();
+    }
 }
