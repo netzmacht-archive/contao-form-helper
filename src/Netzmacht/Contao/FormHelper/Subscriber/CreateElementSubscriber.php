6bde1d589bcb663192b757806c200efae4b300d9
David Molineus <mail@netzmacht.de>
2014-11-21 08:31:09 +0100

    remove wrong "s"

--- src/Netzmacht/Contao/FormHelper/Subscriber/CreateElementSubscriber.php
+++ src/Netzmacht/Contao/FormHelper/Subscriber/CreateElementSubscriber.php
@@ -32,7 +32,7 @@ class CreateElementSubscriber implements EventSubscriberInterface
      *
      * @var array
      */
-    private $textBasedElements = array('password', 'submit', 'tesxt', 'digit', 'email', 'tel', 'url', 'number');
+    private $textBasedElements = array('password', 'submit', 'text', 'digit', 'email', 'tel', 'url', 'number');
 
     /**
      * Get all subscribed events.
